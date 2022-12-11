a7c32997c2debf53a389a4d95d8af11477b6ac89
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-11 11:27:51 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Prints a string to the standard error.
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61, 1, stderr);
+
+	return (1);
+}
+
