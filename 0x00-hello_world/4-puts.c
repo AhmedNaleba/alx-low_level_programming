c1b96a4d262a65fa6da89654fb3709c7e9d6e123
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-11 09:18:02 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,15 @@
 #include <string.h>
 
 /**
- *
  * main - Prints a string
  *
- * Return : 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(str);
 
 	return (0);
