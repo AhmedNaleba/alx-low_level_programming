3ed524ed7cf2a98e6998fbb8152021c2cde3e231
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-14 08:05:34 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0.main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar();
+
+	return (0);
+
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-putchar.c - C program that prints _putchar, followed by a new line.
