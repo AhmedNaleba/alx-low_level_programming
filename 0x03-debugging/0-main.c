ed235a4ffac052437a6665607cfa7a14267889a7
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-15 11:31:24 -0800

    positive_or_negative.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	positive_or_negative(i);
+
+	return (0);
+}
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0-main.c, main.h - Create a file named 0-main.c
