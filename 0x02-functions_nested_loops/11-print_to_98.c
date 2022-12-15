425adade56a62aa62eaee48925349960fab3b175
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-15 10:26:47 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints natural numbers from n - 98
+ * @n: Character to be checked
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+
+	if (n < 98)
+	{
+	while (n <= 98)
+	{
+		printf("%d", n);
+
+	if (n != 98)
+	{
+	printf(", ");
+	}
+		n++;
+	}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+		n--;
+		}
+	}
+	else
+	{
+		printf("98");
+
+	}
+	printf("\n");
+}
+
