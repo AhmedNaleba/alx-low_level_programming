cd93812068cd94f19fdd525c44f91e81eeddbb09
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-16 13:25:28 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Prints line in the terminal
+ * @n: Character to be checked
+ *
+ * Return: 0.
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+
+}
+
