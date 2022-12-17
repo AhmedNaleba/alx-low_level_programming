4a986c1f4f6a29a71c8803e20862877a9a299bc2
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-17 11:26:16 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,24 +11,27 @@ void print_triangle(int size)
 {
 	int a;
 	int b;
-	int c;
 
-	for (a = 0; a < size; a++)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+
+	for (a = 1; a <= size; a++)
 	{
 		for (b = a; b < size; b++)
 			{
 			_putchar(' ');
 			}
-		for (c = 0; c <= a; c++)
+		for (b = 1; b <= a; b++)
 		{
 			_putchar('#');
 		}
 
 		_putchar('\n');
 	}
-		if (size <= 0)
-		{
-			_putchar('\n');
-		}
+	}
 }
 
