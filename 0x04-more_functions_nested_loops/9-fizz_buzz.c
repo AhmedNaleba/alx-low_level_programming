302aba003a843d0b07830e133946521c4c53164b
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-17 07:36:32 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,25 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if ((a % 15) == 0)
+		if (a % 15 == 0)
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
-		else if ((a % 3) == 0)
+		else if (a % 3 == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
-		else if ((a % 5) == 0)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 		else
 		{
 			printf("%d", a);
+			printf(" ");
 		}
 	}
 
