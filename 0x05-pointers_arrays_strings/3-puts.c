c9686a230713cdf6d0d0ecd62d9a3ebff9f98f64
AhmedNaleba <ahmednaleba@gmail.com>
2022-12-20 08:10:35 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: Operand
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+
+}
+
