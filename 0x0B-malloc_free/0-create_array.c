43e8b74341d9e54674afa6d6afeed7521702fd5b
AhmedNaleba <ahmednaleba@gmail.com>
2023-01-09 11:23:35 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,14 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc(sizeof(c) * size);
 	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(c) * size);
+
+	if  (s == NULL)
 	{
 		return (NULL);
 	}
