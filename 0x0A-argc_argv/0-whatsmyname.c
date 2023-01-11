733dd8e94d4aefbf4212303c046b67bd2d419efc
AhmedNaleba <ahmednaleba@gmail.com>
2023-01-11 09:49:43 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints a new line
+ * @argc: Int operand
+ * @argv: Char operand
+ *
+ * Return: 0
+ */
+
+int main(int __attribute__((__unused__))argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
