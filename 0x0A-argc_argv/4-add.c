4ae6baef8d1b1e03f1533f7bc26d560ba627fba3
AhmedNaleba <ahmednaleba@gmail.com>
2023-01-13 06:39:21 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Adds intergers and checks for special xters
@@ -12,23 +11,22 @@
 
 int main(int argc, char *argv[])
 {
-	int a;
+	int a, b;
 	int sum = 0;
 
-	if (argc == 0)
-	{
-		printf("0");
-	}
-	if (argc != isdigit(a))
-	{
-		printf("Error\n");
-		return (1);
-	}
 	for (a = 1; a < argc; a++)
 	{
+		for (b = 0; b < argv[a][b]; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		sum = sum + atoi(argv[a]);
 	}
-		printf("%d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
 
